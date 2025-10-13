522dd33d7f8babeb74df762e07612d5bf5993aa9
skorykov-ivan <skorykov.ivan@gmail.com>
2025-10-13 18:26:01 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -6,8 +6,8 @@ select
 	s.campaign as utm_campaign,
 	coalesce(sum(vk.daily_spent), 0) + coalesce(sum(ya.daily_spent), 0) as total_cost,
 	count(distinct l.lead_id) as leads_count,
-	count(*) filter(where l.status_id = 142) as purchases_count,
-	coalesce(sum(l.amount), 0) as revenue
+	count(distinct l.lead_id) filter(where l.status_id = 142) as purchases_count,
+	coalesce(sum(distinct case when l.status_id = 142 then l.amount else 0 end), 0) as revenue
 from sessions as s
 left join leads as l on s.visitor_id = l.visitor_id
 left join vk_ads as vk on date(s.visit_date) = vk.campaign_date and
@@ -23,7 +23,5 @@ left join ya_ads as ya on date(s.visit_date) = ya.campaign_date and
 where medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 group by date(s.visit_date), s.source, s.medium, s.campaign
 order by visit_date, visitors_count desc, utm_source, utm_medium, utm_campaign, revenue desc nulls last limit 15;
-
-
 
 

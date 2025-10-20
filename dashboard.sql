e3e88e1bf15f3b5b5448b09077cbfcc1fb9dc001
skorykov-ivan <skorykov.ivan@gmail.com>
2025-10-20 13:09:53 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -1,7 +1,5 @@
 --------- Основная таблица для запросов с платной рекламой
 --------- нет пометки'(без оплаты за рекламу)'
---------- Оборачиваем всю таблицу в CTE tbl_answ
--- with tbl_answ as (
 with last_visits as (
     select
         visitor_id,
@@ -51,34 +49,34 @@ tbl_ads as (
         sum(daily_spent) as total_cost
     from ya_ads
     group by utm_source, utm_medium, utm_campaign, date(campaign_date)
-)
+),
 
-select
-    t_ag.visit_date,
-    t_ag.visitors_count,
-    t_ag.utm_source,
-    t_ag.utm_medium,
-    t_ag.utm_campaign,
-    ads.total_cost,
-    t_ag.leads_count,
-    t_ag.purchases_count,
-    t_ag.revenue
-from tbl_aggr as t_ag
-left join tbl_ads as ads
-    on
-        t_ag.visit_date = ads.campaign_date
-        and t_ag.utm_source = ads.utm_source
-        and t_ag.utm_medium = ads.utm_medium
-        and t_ag.utm_campaign = ads.utm_campaign
-where t_ag.utm_source in ('yandex', 'vk')
-order by
-    t_ag.revenue desc nulls last, t_ag.visit_date asc,
-    t_ag.visitors_count desc, t_ag.utm_source asc,
-    t_ag.utm_medium asc, t_ag.utm_campaign asc;
+aggregate_last_paid_click as (
+    select
+        t_ag.visit_date,
+        t_ag.visitors_count,
+        t_ag.utm_source,
+        t_ag.utm_medium,
+        t_ag.utm_campaign,
+        ads.total_cost,
+        t_ag.leads_count,
+        t_ag.purchases_count,
+        t_ag.revenue
+    from tbl_aggr as t_ag
+    left join tbl_ads as ads
+        on
+            t_ag.visit_date = ads.campaign_date
+            and t_ag.utm_source = ads.utm_source
+            and t_ag.utm_medium = ads.utm_medium
+            and t_ag.utm_campaign = ads.utm_campaign
+    where t_ag.utm_source in ('yandex', 'vk')
+    order by
+        t_ag.revenue desc nulls last, t_ag.visit_date asc,
+        t_ag.visitors_count desc, t_ag.utm_source asc,
+        t_ag.utm_medium asc, t_ag.utm_campaign asc
+)
 --------- Основная таблица для запросов без оплаты за рекламу
 --------- с пометкой '(без оплаты за рекламу)'
---------- оборачиваем всю таблицу в CTE tbl_free
--- with tbl_free as (
 with last_visits as (
     select
         visitor_id,
@@ -86,23 +84,25 @@ with last_visits as (
     from sessions
     where source in ('google', 'organic')
     group by visitor_id
-)
+),
 
-select
-    s.source as utm_source,
-    date(lv.last_date) as visit_date,
-    count(distinct s.visitor_id) as visitors_count,
-    count(distinct l.lead_id) as leads_count,
-    count(distinct s.visitor_id) filter (
-        where l.status_id = 142
-    ) as purchases_count,
-    sum(l.amount) as revenue
-from last_visits as lv
-inner join sessions as s
-    on lv.visitor_id = s.visitor_id and lv.last_date = s.visit_date
-left join leads as l
-    on lv.visitor_id = l.visitor_id and lv.last_date <= l.created_at
-group by date(lv.last_date), s.source;
+tbl_free as (
+    select
+        s.source as utm_source,
+        date(lv.last_date) as visit_date,
+        count(distinct s.visitor_id) as visitors_count,
+        count(distinct l.lead_id) as leads_count,
+        count(distinct s.visitor_id) filter (
+            where l.status_id = 142
+        ) as purchases_count,
+        sum(l.amount) as revenue
+    from last_visits as lv
+    inner join sessions as s
+        on lv.visitor_id = s.visitor_id and lv.last_date = s.visit_date
+    left join leads as l
+        on lv.visitor_id = l.visitor_id and lv.last_date <= l.created_at
+    group by date(lv.last_date), s.source
+)
 --------- 1 таблица по дням + 3 таблица по месяцам с фильтром в superset
 select
     visit_date,
@@ -110,10 +110,13 @@ select
     sum(visitors_count) as visitors_count,
     sum(leads_count) as leads_count,
     sum(purchases_count) as purchases_count
-from tbl_answ
+from aggregate_last_paid_click
 group by visit_date, utm_source
 order by
-    visit_date asc, visitors_count desc, leads_count desc, purchases_count desc;
+    visit_date asc,
+    visitors_count desc,
+    leads_count desc,
+    purchases_count desc;
 --------- 1.1 таблица по дням (без оплаты за рекламу)
 select
     visit_date,
@@ -140,7 +143,7 @@ select
     sum(visitors_count) as visitors_count,
     sum(leads_count) as leads_count,
     sum(purchases_count) as purchases_count
-from tbl_answ
+from aggregate_last_paid_click
 group by wkd, extract(isodow from visit_date), utm_source
 order by
     extract(isodow from visit_date), visitors_count desc, leads_count desc;
@@ -199,21 +202,21 @@ from tbl_free;
 select
     'Пользователи' as sta_ge,
     sum(visitors_count) as count_all
-from tbl_answ
+from aggregate_last_paid_click
 
 union all
 
 select
     'Лиды' as sta_ge,
     sum(leads_count) as leads_count
-from tbl_answ
+from aggregate_last_paid_click
 
 union all
 
 select
     'Покупатели' as sta_ge,
     sum(purchases_count) as purchases_count
-from tbl_answ;
+from aggregate_last_paid_click;
 --------- 7 таблица - расходы и доходы по дням недели
 select
     extract(isodow from visit_date) as sort,
@@ -229,7 +232,7 @@ select
     end as russian_day,
     sum(total_cost) as total_cost,
     coalesce(sum(revenue), 0) as revenue
-from tbl_answ
+from aggregate_last_paid_click
 group by wkd, extract(isodow from visit_date)
 order by extract(isodow from visit_date);
 --------- 8 таблица - расходы по разным каналам в динамике
@@ -237,7 +240,7 @@ select
     visit_date,
     utm_source,
     sum(total_cost) as daily_spent
-from tbl_answ
+from aggregate_last_paid_click
 group by visit_date, utm_source
 having sum(total_cost) > 0
 order by visit_date asc, daily_spent desc;
@@ -247,7 +250,7 @@ select
     utm_source,
     sum(total_cost) as total_cost,
     sum(revenue) as revenue
-from tbl_answ
+from aggregate_last_paid_click
 group by utm_source
 order by sum(total_cost) desc, sum(revenue) desc limit 4;
 
@@ -258,22 +261,103 @@ select
     round(sum(total_cost) / sum(leads_count), 2) as cpl,
     round(sum(total_cost) / sum(purchases_count), 2) as cppu,
     round((sum(revenue) - sum(total_cost)) / sum(total_cost) * 100, 2) as roi
-from tbl_answ
+from aggregate_last_paid_click
 group by utm_source;
---------- 11 таблица - cpu, cpl, cppu, roi по utm_campaign
------ нужно поставить запятую после CTE tbl_answ и убрать тут with
-with tbl_cost_revenue_utm_campaign as (
+--------- 11 таблица - cpu, cpl, cppu, roi по utm_campaign + 90% закрытие лидов
+----- целый запрос, добавлять таблицы не нужно
+with last_visits as (
+    select
+        visitor_id,
+        max(visit_date) as last_date
+    from sessions
+    where medium != 'organic'
+    group by visitor_id
+),
+
+tbl_aggr as (
+    select
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        percentile_disc(0.9) within group (
+            order by date(l.created_at) - date(visit_date)
+        ) as close_leads_90perc, -- тут считаем закрытие 90% лидов
+        date(lv.last_date) as visit_date,
+        count(distinct s.visitor_id) as visitors_count,
+        count(distinct l.lead_id) as leads_count,
+        count(distinct s.visitor_id) filter (
+            where l.status_id = 142
+        ) as purchases_count,
+        sum(l.amount) as revenue
+    from last_visits as lv
+    inner join sessions as s
+        on lv.visitor_id = s.visitor_id and lv.last_date = s.visit_date
+    left join leads as l
+        on lv.visitor_id = l.visitor_id and lv.last_date <= l.created_at
+    group by date(lv.last_date), s.source, s.medium, s.campaign
+),
+
+tbl_ads as (
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from vk_ads
+    group by utm_source, utm_medium, utm_campaign, date(campaign_date)
+
+    union
+
+    select
+        date(campaign_date) as campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by utm_source, utm_medium, utm_campaign, date(campaign_date)
+),
+
+aggregate_last_paid_click as (
+    select
+        t_ag.visit_date,
+        t_ag.visitors_count,
+        t_ag.utm_source,
+        t_ag.utm_medium,
+        t_ag.utm_campaign,
+        ads.total_cost,
+        t_ag.leads_count,
+        t_ag.purchases_count,
+        t_ag.revenue,
+        close_leads_90perc
+    from tbl_aggr as t_ag
+    left join tbl_ads as ads
+        on
+            t_ag.visit_date = ads.campaign_date
+            and t_ag.utm_source = ads.utm_source
+            and t_ag.utm_medium = ads.utm_medium
+            and t_ag.utm_campaign = ads.utm_campaign
+    where t_ag.utm_source in ('yandex', 'vk')
+    order by
+        t_ag.revenue desc nulls last, t_ag.visit_date asc,
+        t_ag.visitors_count desc, t_ag.utm_source asc,
+        t_ag.utm_medium asc, t_ag.utm_campaign asc
+),
+
+tbl_cost_revenue_utm_campaign as (
     select
         utm_source,
         utm_medium,
         utm_campaign,
+        close_leads_90perc,
         sum(visitors_count) as visitors_count,
         sum(leads_count) as leads_count,
         sum(purchases_count) as purchases_count,
         coalesce(sum(total_cost), 0) as total_cost,
         coalesce(sum(revenue), 0) as revenue
-    from tbl_answ
-    group by utm_source, utm_medium, utm_campaign
+    from aggregate_last_paid_click
+    group by utm_source, utm_medium, utm_campaign, close_leads_90perc
     having coalesce(sum(total_cost), 0) > 0
     order by
         visitors_count desc,
@@ -282,6 +366,7 @@ with tbl_cost_revenue_utm_campaign as (
 )
 
 select
+	row_number() over (order by (revenue - total_cost) desc) as place,
     utm_source,
     utm_medium,
     utm_campaign,
@@ -306,6 +391,7 @@ select
         when total_cost = 0 then 0
         else round((revenue - total_cost) / total_cost * 100, 2)
     end as roi,
-    (revenue - total_cost) as net_profit
+    (revenue - total_cost) as net_profit,
+    close_leads_90perc
 from tbl_cost_revenue_utm_campaign
 order by net_profit desc;

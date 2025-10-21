762bff3ea559d5fbafc47d72ae9f0530cb33cb7b
skorykov-ivan <skorykov.ivan@gmail.com>
2025-10-21 15:26:40 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,15 +1,7 @@
-with last_visits as (
+with rank_date as (
     select
-        visitor_id,
-        max(visit_date) as last_date
-    from sessions
-    where medium != 'organic'
-    group by visitor_id
-)
-
-select
     s.visitor_id,
-    lv.last_date as visit_date,
+    s.visit_date,
     s.source as utm_source,
     s.medium as utm_medium,
     s.campaign as utm_campaign,
@@ -17,14 +9,28 @@ select
     l.created_at,
     l.amount,
     l.closing_reason,
-    l.status_id
+    l.status_id,
+    row_number() over (partition by s.visitor_id order by s.visit_date desc) as rn
 from sessions as s
-inner join last_visits as lv
-    on s.visitor_id = lv.visitor_id and s.visit_date = lv.last_date
 left join leads as l
     on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
 where s.medium != 'organic'
+)
+
+select
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from rank_date
+where rn = 1
 order by
-    l.amount desc nulls last, visit_date asc, utm_source asc,
+    amount desc nulls last, visit_date asc, utm_source asc,
     utm_medium asc, utm_campaign asc
 limit 10;

9c30fc25db8bfa2c818b2a487723aeb1f253102a
skorykov-ivan <skorykov.ivan@gmail.com>
2025-10-21 15:54:48 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,20 +1,22 @@
 with rank_date as (
     select
-    s.visitor_id,
-    s.visit_date,
-    s.source as utm_source,
-    s.medium as utm_medium,
-    s.campaign as utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id,
-    row_number() over (partition by s.visitor_id order by s.visit_date desc) as rn
-from sessions as s
-left join leads as l
-    on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
-where s.medium != 'organic'
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number() over (
+            partition by s.visitor_id order by s.visit_date desc
+        ) as rn
+    from sessions as s
+    left join leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    where s.medium != 'organic'
 )
 
 select
